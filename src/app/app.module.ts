800db7adccc652343f85f6f574a20faab9e68461
DangCao1999 <cao.nvd2246@sinhvien.hoasen.edu.vn>
2022-03-04 21:34:46 +0700

    create template service and model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule} from '@nebular/theme';
+import { NbThemeModule } from '@nebular/theme';
 import { StoreModule } from '@ngrx/store';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -17,8 +22,14 @@ import { StoreModule } from '@ngrx/store';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     StoreModule.forRoot({}, {}),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService, UserTrackingService
+  ],
   bootstrap: [AppComponent],
 
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'gdsc-gateway',
+    appId: '1:162603779372:web:1aafc4dbc23a4e0ef7d889',
+    storageBucket: 'gdsc-gateway.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyBaTTiFiDxcAM6y2P3dNMCfEVgEg3WqVvA',
+    authDomain: 'gdsc-gateway.firebaseapp.com',
+    messagingSenderId: '162603779372',
+    measurementId: 'G-JVJ5SNZB84',
+  },
   production: false
 };
 
